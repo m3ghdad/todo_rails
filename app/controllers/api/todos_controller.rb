e99aa6bf066a21e2d6f4d71f395fbcc5fb15e5db
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-14 10:43:25 -0800

    Adds model for todos and controller nested in api for todos controller with show, create, destroy and index

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,39 @@
+class Api::TodosController < ApplicationController
+  def create
+    @todo = Todo.new(todos_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todos_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    render json: @todos
+  end
+
+  private
+  def todos_params
+    params.require(:todos).permit(:title, :body, :done)
+  end
+end
